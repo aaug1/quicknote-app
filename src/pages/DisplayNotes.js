1bda680a56d3dc2ed441591fb0038333ce01e76c
Aidan Aug <aidanaug@Aidans-MacBook-Pro.local>
2022-04-18 20:34:01 -0400

    finished the quicknote app finally

--- src/pages/DisplayNotes.js
+++ src/pages/DisplayNotes.js
@@ -1,36 +1,57 @@
 import { List, Fab, withStyles } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
+import { Component } from "react";
 import Note from "../components/Note";
+import Search from "../components/Search";
 import { Link } from "react-router-dom";
 
-
 const styles = {
-  fab: {
-    position: 'absolute',
-    bottom: "2rem",
-    right: "2rem",
+    fab: {
+      position: 'absolute',
+      bottom: "2rem",
+      right: "2rem",
+    }
+  };
+
+class DisplayNotes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+    };
   }
-};
 
-function DisplayNotes(props) {
-  const { notes, deleteNote, classes } = props;
-  return (
-    <>
-      <List>
-        {notes.map((note, index) => {
-          return <Note note={note} key={index} deleteNote={deleteNote} />;
-        })}
-      </List>
-      <Link to="/add"> {/* The Link component is similar to the HTML anchor element (<a>). */}
-        <Fab
-          aria-label={"Add"}
-          className={classes.fab}>
-          <Add />
-        </Fab>
-      </Link>
+  updateQuery = (query) => {
+    this.setState({ query });
+  };
 
-    </>
-  );
+  includes = (note) => {
+    const query = this.state.query.trim().toLowerCase();
+    return (
+      query === "" ||
+      note.title.toLowerCase().includes(query) ||
+      note.text.toLowerCase().includes(query)
+    );
+  };
+
+  render() {
+    const { notes, deleteNote, classes } = this.props;
+    return (
+      <>
+        <Search query={this.state.query} updateQuery={this.updateQuery} />
+        <List>
+          {notes.filter(this.includes).map((note, index) => {
+            return <Note note={note} key={index} deleteNote={deleteNote} />;
+          })}
+        </List>
+        <Link to="/add">
+          <Fab aria-label={"Add"} className={classes.fab}>
+            <Add />
+          </Fab>
+        </Link>
+      </>
+    );
+  }
 }
 
 export default withStyles(styles)(DisplayNotes);
